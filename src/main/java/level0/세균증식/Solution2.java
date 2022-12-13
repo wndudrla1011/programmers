57cd84440868ac2249c9ca05ff3e12777bfb0e77
wndudrla1011 <wndudrla1011@naver.com>
2022-12-13 17:41:14 +0900

    세균 증식 완료

---
+++ src/main/java/level0/세균증식/Solution2.java
@@ -0,0 +1,15 @@
+package level0.세균증식;
+
+public class Solution2 {
+
+    public int solution(int n, int t) {
+        return n << t;
+    }
+
+    public static void main(String[] args) {
+        Solution2 s = new Solution2();
+        int result = s.solution(7, 15);
+        System.out.println(result);
+    }
+
+}
