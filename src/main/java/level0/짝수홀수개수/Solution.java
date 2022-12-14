d5b454ba91ea7e59d1353b60440d08238497e47b
wndudrla1011 <wndudrla1011@naver.com>
2022-12-14 16:55:16 +0900

    짝수와 홀수의 개수 완료

---
+++ src/main/java/level0/짝수홀수개수/Solution.java
@@ -0,0 +1,30 @@
+package level0.짝수홀수개수;
+
+import java.util.Arrays;
+
+public class Solution {
+
+    public int[] solution(int[] num_list) {
+        int[] answer = new int[2];
+
+        long even = Arrays.stream(num_list)
+                .filter(n -> n % 2 == 0)
+                .count();
+
+        long odd = Arrays.stream(num_list)
+                .filter(n -> n % 2 != 0)
+                .count();
+
+        answer[0] = (int) even;
+        answer[1] = (int) odd;
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        int[] result = s.solution(new int[]{1, 3, 5, 7});
+        System.out.println("result = " + Arrays.toString(result));
+    }
+
+}
