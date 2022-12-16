6323c8c7a6e7ca995774dcd7e0aa8adc9a4f0a56
wndudrla1011 <wndudrla1011@naver.com>
2022-12-16 17:32:00 +0900

    몫 구하기 완료

---
+++ src/main/java/level0/몫구하기/Solution.java
@@ -0,0 +1,14 @@
+package level0.몫구하기;
+
+public class Solution {
+
+    public int solution(int num1, int num2) {
+        return num1 / num2;
+    }
+
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        System.out.println(s.solution(10, 5));
+    }
+
+}
