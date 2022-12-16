d15848d93077508dab4669c0ff2a4ba2d97aef06
wndudrla1011 <wndudrla1011@naver.com>
2022-12-16 17:27:48 +0900

    진료 순서 정하기 완료

---
+++ src/main/java/level0/진료순서정하기/Solution2.java
@@ -0,0 +1,19 @@
+package level0.진료순서정하기;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class Solution2 {
+
+    public int[] solution(int[] e) {
+        return Arrays.stream(e)
+                .map(i -> Arrays.stream(e).boxed().sorted(Comparator.reverseOrder()).collect(Collectors.toList()).indexOf(i) + 1)
+                .toArray();
+    }
+
+    public static void main(String[] args) {
+        Solution2 s = new Solution2();
+        System.out.println(Arrays.toString(s.solution(new int[]{3, 76, 24})));
+    }
+
+}
