0af83ed6ed3cacdd52ff2c7ea82305233e9d2c78
wndudrla1011 <wndudrla1011@naver.com>
2022-12-20 11:32:12 +0900

    겹치는 선분의 길이 완료

--- src/main/java/level0/겹치는선분의길이/Solution.java
+++ src/main/java/level0/겹치는선분의길이/Solution.java
@@ -3,26 +3,23 @@ package level0.겹치는선분의길이;
 public class Solution {
 
     public int solution(int[][] lines) {
+        int[] arr = new int[200];
         int answer = 0;
 
-        for (int i = 0; i < lines.length - 1; i++) {
-            for (int j = i + 1; j < lines.length; j++) {
-                if (lines[i][0] < lines[j][0]) {
-                    answer += lines[i][1] > lines[j][0] ? lines[i][1] - lines[j][0] : 0;
-                } else if (lines[i][0] > lines[j][0]) {
-                    answer += lines[j][1] > lines[i][0] ? lines[j][1] - lines[i][0] : 0;
-                } else {
-                    answer += lines[i][1] < lines[j][1] ? lines[i][1] - lines[i][0] + 1 : lines[j][1] - lines[j][0] + 1;
-                }
-            }
-        }
+        for(int i = 0; i < lines.length; i++)
+            for(int j = lines[i][0] + 100; j < lines[i][1] + 100; j++)
+                arr[j]++;
+
+        for(int i = 0; i < 200; i++)
+            if(arr[i] > 1)
+                answer++;
 
         return answer;
     }
 
     public static void main(String[] args) {
         Solution s = new Solution();
-        System.out.println(s.solution(new int[][]{{0, 5}, {3, 9}, {1, 10}}));
+        System.out.println(s.solution(new int[][]{{0, 2}, {-3, -1}, {-2, 1}}));
     }
 
 }
