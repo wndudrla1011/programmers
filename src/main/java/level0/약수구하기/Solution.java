f0a9c36d1908738fde4bef162c173f40e8718664
wndudrla1011 <wndudrla1011@naver.com>
2022-12-20 16:21:03 +0900

    약수 구하기 완료

---
+++ src/main/java/level0/약수구하기/Solution.java
@@ -0,0 +1,25 @@
+package level0.약수구하기;
+
+import java.util.*;
+
+public class Solution {
+
+    public int[] solution(int n) {
+        ArrayList<Integer> answer = new ArrayList<>();
+
+        for (int i = 1; i <= Math.sqrt(n); i++) {
+            if (n % i == 0) {
+                answer.add(i);
+                answer.add(n / i);
+            }
+        }
+
+        return answer.stream().distinct().sorted().mapToInt(Integer::intValue).toArray();
+    }
+
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        System.out.println(Arrays.toString(s.solution(10000)));
+    }
+
+}
