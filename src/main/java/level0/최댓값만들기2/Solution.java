40c65b78f164cefce2f88391d7c486fb6895e297
wndudrla1011 <wndudrla1011@naver.com>
2022-12-21 15:32:42 +0900

    최대값 만들기(2), 중복된 문자 제거 완료

---
+++ src/main/java/level0/최댓값만들기2/Solution.java
@@ -0,0 +1,22 @@
+package level0.최댓값만들기2;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class Solution {
+
+    public int solution(int[] numbers) {
+        List<Integer> list = Arrays.stream(numbers).boxed().sorted().collect(Collectors.toList());
+
+        int negativeMax = list.get(0) * list.get(1);
+        int positiveMax = list.get(numbers.length - 1) * list.get(numbers.length - 2);
+
+        return Math.max(negativeMax, positiveMax);
+    }
+
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        System.out.println(s.solution(new int[]{0, -31, 24, 10, 1, 9}));
+    }
+
+}
