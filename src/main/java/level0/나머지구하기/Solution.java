0a3adfa7e75017faca38f708b0070de10392b15e
wndudrla1011 <wndudrla1011@naver.com>
2022-12-21 12:31:22 +0900

    나머지 구하기 완료

---
+++ src/main/java/level0/나머지구하기/Solution.java
@@ -0,0 +1,14 @@
+package level0.나머지구하기;
+
+public class Solution {
+
+    public int solution(int num1, int num2) {
+        return num1 % num2;
+    }
+
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        System.out.println(s.solution(3, 2));
+    }
+
+}
