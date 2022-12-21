a5344311af31239bc27bc8de8eb117712d844d46
wndudrla1011 <wndudrla1011@naver.com>
2022-12-21 17:01:37 +0900

    공 던지기 완료

---
+++ src/main/java/level0/공던지기/Solution2.java
@@ -0,0 +1,16 @@
+package level0.공던지기;
+
+public class Solution2 {
+
+    public int solution(int[] numbers, int k) {
+        return (k * 2 - 1) % numbers.length; //번호 규칙1
+//        return (k - 1) * 2 % numbers.length + 1; //번호 규칙2
+//        return numbers[((1 + (k - 1) * 2) % numbers.length) - 1]; //인덱스 규칙
+    }
+
+    public static void main(String[] args) {
+        Solution2 s = new Solution2();
+        System.out.println(s.solution(new int[]{1, 2, 3, 4}, 2));
+    }
+
+}
