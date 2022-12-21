628295ac0bce4c1f6465fd425ee4618561082a56
wndudrla1011 <wndudrla1011@naver.com>
2022-12-21 10:35:50 +0900

    개미 군단 완료

---
+++ src/main/java/level0/개미군단/Solution2.java
@@ -0,0 +1,18 @@
+package level0.개미군단;
+
+public class Solution2 {
+
+    public int solution(int hp) {
+        int ant_5 = hp / 5;
+        int ant_3 = (hp % 5) / 3;
+        int ant_1 = (hp % 5) % 3;
+
+        return ant_5 + ant_3 + ant_1;
+    }
+
+    public static void main(String[] args) {
+        Solution2 s = new Solution2();
+        System.out.println(s.solution(24));
+    }
+
+}
