c383f1b778f5893c1005dcccae8aca18412b0f42
wndudrla1011 <wndudrla1011@naver.com>
2022-12-23 12:03:43 +0900

    삼각형의 완성조건(1) 완료

---
+++ src/main/java/level0/삼각형의완성조건1/Solution.java
@@ -0,0 +1,24 @@
+package level0.삼각형의완성조건1;
+
+import java.util.Arrays;
+import java.util.stream.IntStream;
+
+public class Solution {
+
+    public int solution(int[] sides) {
+        Arrays.sort(sides);
+        int max = sides[sides.length - 1];
+        int others = IntStream.rangeClosed(0, sides.length - 2).map(s -> sides[s]).sum();
+
+        if (max < others || others == 0)
+            return 1;
+        else
+            return 2;
+    }
+
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        System.out.println(s.solution(new int[]{4, 9, 9}));
+    }
+
+}
