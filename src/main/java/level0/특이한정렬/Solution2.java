4a9ebfa05f4d24984aeaf3ad778dd7eed86f59fd
wndudrla1011 <wndudrla1011@naver.com>
2022-12-23 22:11:09 +0900

    특이한 정렬 완료

---
+++ src/main/java/level0/특이한정렬/Solution2.java
@@ -0,0 +1,20 @@
+package level0.특이한정렬;
+
+import java.util.*;
+
+public class Solution2 {
+
+    public int[] solution(int[] numlist, int n) {
+        return Arrays.stream(numlist)
+                .boxed()
+                .sorted((a, b) -> Math.abs(a - n) == Math.abs(b - n) ? b.compareTo(a) : Integer.compare(Math.abs(a - n), Math.abs(b - n)))
+                .mapToInt(Integer::intValue)
+                .toArray();
+    }
+
+    public static void main(String[] args) {
+        Solution2 s = new Solution2();
+        System.out.println(Arrays.toString(s.solution(new int[]{10000, 20, 36, 47, 40, 6, 10, 7000}, 30)));
+    }
+
+}
