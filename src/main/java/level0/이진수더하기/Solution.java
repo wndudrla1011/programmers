e0a9c29712528fced003d41b6e153c1545a7a5fb
wndudrla1011 <wndudrla1011@naver.com>
2022-12-23 11:15:04 +0900

    이진수 더하기 완료

---
+++ src/main/java/level0/이진수더하기/Solution.java
@@ -0,0 +1,32 @@
+package level0.이진수더하기;
+
+public class Solution {
+
+    public String solution(String bin1, String bin2) {
+        StringBuilder answer = new StringBuilder();
+        int binSum = 0;
+
+        int i = 1;
+        for (String bin : bin1.split("")) {
+            binSum += Integer.parseInt(bin) * (int) Math.pow(2, bin1.length() - i++);
+        }
+
+        int j = 1;
+        for (String bin : bin2.split("")) {
+            binSum += Integer.parseInt(bin) * (int) Math.pow(2, bin2.length() - j++);
+        }
+
+        while (binSum > 0) {
+            answer.append(binSum % 2);
+            binSum /= 2;
+        }
+
+        return answer.length() != 0 ? answer.reverse().toString() : "0";
+    }
+
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        System.out.println(s.solution("10", "11"));
+    }
+
+}
---
+++ src/main/java/level0/이진수더하기/Solution2.java
@@ -0,0 +1,14 @@
+package level0.이진수더하기;
+
+public class Solution2 {
+
+    public String solution(String bin1, String bin2) {
+        return Integer.toString(Integer.parseInt(bin1, 2) + Integer.parseInt(bin2, 2), 2);
+    }
+
+    public static void main(String[] args) {
+        Solution2 s = new Solution2();
+        System.out.println(s.solution("10", "11"));
+    }
+
+}
