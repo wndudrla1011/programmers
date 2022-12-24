e265f4351208f81dc3199f7375ee102efaa9f4c7
wndudrla1011 <wndudrla1011@naver.com>
2022-12-24 12:32:43 +0900

    A로 B 만들기 완료

---
+++ src/main/java/level0/A로B만들기/Solution.java
@@ -0,0 +1,20 @@
+package level0.A로B만들기;
+
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
+public class Solution {
+
+    public int solution(String before, String after) {
+        String a = Arrays.stream(before.split("")).sorted().collect(Collectors.joining());
+        String b = Arrays.stream(after.split("")).sorted().collect(Collectors.joining());
+
+        return a.equals(b) ? 1 : 0;
+    }
+
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        System.out.println(s.solution("allpe", "apple"));
+    }
+
+}
