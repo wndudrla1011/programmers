868889bc2a55dfb10b92c5ee62e058cf50816186
wndudrla1011 <wndudrla1011@naver.com>
2022-12-24 11:55:45 +0900

    양꼬치 완료

---
+++ src/main/java/level0/양꼬치/Solution.java
@@ -0,0 +1,14 @@
+package level0.양꼬치;
+
+public class Solution {
+
+    public int solution(int n, int k) {
+        return 12000 * n + 2000 * (k - n / 10);
+    }
+
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        System.out.println(s.solution(10, 3));
+    }
+
+}
