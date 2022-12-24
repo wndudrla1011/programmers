e59ce4914d1feaf49bb405cc07cb2c41fc8e12a7
wndudrla1011 <wndudrla1011@naver.com>
2022-12-24 12:19:40 +0900

    편지 완료

---
+++ src/main/java/level0/편지/Solution.java
@@ -0,0 +1,14 @@
+package level0.편지;
+
+public class Solution {
+
+    public int solution(String message) {
+        return message.length() * 2;
+    }
+
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        System.out.println(s.solution("happy birthday!"));
+    }
+
+}
