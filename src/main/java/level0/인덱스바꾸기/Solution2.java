9e87caa1cf9854b47d4079158eed6cc579b943bd
wndudrla1011 <wndudrla1011@naver.com>
2022-12-26 15:13:38 +0900

    인덱스 바꾸기 완료

---
+++ src/main/java/level0/인덱스바꾸기/Solution2.java
@@ -0,0 +1,23 @@
+package level0.인덱스바꾸기;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Solution2 {
+
+    public String solution(String my_string, int num1, int num2) {
+        List<String> list = Arrays.stream(my_string.split("")).collect(Collectors.toList());
+
+        Collections.swap(list, num1, num2);
+
+        return String.join("", list);
+    }
+
+    public static void main(String[] args) {
+        Solution2 s = new Solution2();
+        System.out.println(s.solution("hello", 1, 2));
+    }
+
+}
