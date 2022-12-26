1dd6869948de1dbf1d3733bf66fe9cbeed32e906
wndudrla1011 <wndudrla1011@naver.com>
2022-12-26 12:03:07 +0900

    문자 반복 출력하기 완료

---
+++ src/main/java/level0/문자반복출력하기/Solution.java
@@ -0,0 +1,17 @@
+package level0.문자반복출력하기;
+
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
+public class Solution {
+
+    public String solution(String my_string, int n) {
+        return Arrays.stream(my_string.split("")).map(s -> s.repeat(n)).collect(Collectors.joining());
+    }
+
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        System.out.println(s.solution("hello", 3));
+    }
+
+}
