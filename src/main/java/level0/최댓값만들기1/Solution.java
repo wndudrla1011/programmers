eda5daf7b85760703ecca1917f39b6a3b81e153a
wndudrla1011 <wndudrla1011@naver.com>
2022-12-26 15:32:49 +0900

    최댓값 만들기(1) 완료

---
+++ src/main/java/level0/최댓값만들기1/Solution.java
@@ -0,0 +1,17 @@
+package level0.최댓값만들기1;
+
+import java.util.Arrays;
+
+public class Solution {
+
+    public int solution(int[] numbers) {
+        Arrays.sort(numbers);
+        return numbers[numbers.length - 1] * numbers[numbers.length - 2];
+    }
+
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        System.out.println(s.solution(new int[]{0, 31, 24, 10, 1, 9}));
+    }
+
+}
