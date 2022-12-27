2ff937eb40900961a679b32304d380847d24accc
wndudrla1011 <wndudrla1011@naver.com>
2022-12-27 12:51:24 +0900

    제곱수 판별하기 풀이 추가

---
+++ src/main/java/level0/제곱수판별하기/Solution2.java
@@ -0,0 +1,16 @@
+package level0.제곱수판별하기;
+
+public class Solution2 {
+
+    public int solution(int n) {
+        String[] slice = String.valueOf(Math.sqrt(n)).split("\\.");
+        return slice[1].equals("0") ? 1 : 2;
+    }
+
+    public static void main(String[] args) {
+        Solution2 s = new Solution2();
+        int result = s.solution(976);
+        System.out.println(result);
+    }
+
+}
