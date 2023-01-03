3bfc1ae072ce90ee918ae0d7d5457501e9c42589
wndudrla1011 <wndudrla1011@naver.com>
2023-01-03 15:45:35 +0900

    두 정수 사이의 합 완료

---
+++ src/main/java/level1/두정수사이의합/Solution3.java
@@ -0,0 +1,20 @@
+package level1.두정수사이의합;
+
+public class Solution3 {
+
+    public long solution(int a, int b) {
+        long answer = 0;
+
+        for (int i = (Math.min(a, b)); i <= (Math.max(a, b)); i++) {
+            answer += i;
+        }
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Solution3 s = new Solution3();
+        System.out.println(s.solution(3, 5));
+    }
+
+}
