de89a84a73007adddbe32a48decbc10d0ec48a49
wndudrla1011 <wndudrla1011@naver.com>
2023-01-09 17:18:31 +0900

    k번째수 완료

---
+++ src/main/java/level1/k번째수/Solution.java
@@ -0,0 +1,25 @@
+package level1.k번째수;
+
+import java.util.Arrays;
+
+public class Solution {
+
+    public int[] solution(int[] array, int[][] commands) {
+        int[] answer = new int[commands.length];
+
+        for (int i = 0; i < commands.length; i++) {
+            int[] temp = Arrays.copyOfRange(array, commands[i][0] - 1, commands[i][1]);
+            Arrays.sort(temp);
+            answer[i] = temp[commands[i][2] - 1];
+        }
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        System.out.println(Arrays.toString(s.solution(new int[]{1, 5, 2, 6, 3, 7, 4},
+                new int[][]{{2, 5, 3}, {4, 4, 1}, {1, 7, 3}})));
+    }
+
+}
