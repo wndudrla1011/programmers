35cee071ec8594130735968841176118c0012488
wndudrla1011 <wndudrla1011@naver.com>
2023-01-09 12:38:58 +0900

    문자열 내 마음대로 정렬하기 완료

---
+++ src/main/java/level1/문자열내마음대로정렬하기/Solution2.java
@@ -0,0 +1,33 @@
+package level1.문자열내마음대로정렬하기;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+public class Solution2 {
+
+    public String[] solution(String[] s, int n) {
+        String[] answer = {};
+        List<String> arr = new ArrayList<>();
+
+        for (int i = 0; i < s.length; i++) {
+            arr.add("" + s[i].charAt(n) + s[i]);
+        }
+
+        Collections.sort(arr);
+
+        answer = new String[arr.size()];
+        for (int i = 0; i < arr.size(); i++) {
+            answer[i] = arr.get(i).substring(1);
+        }
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Solution2 s = new Solution2();
+        System.out.println(Arrays.toString(s.solution(new String[]{"bbc", "dba", "abcd", "ceo"}, 2)));
+    }
+
+}
