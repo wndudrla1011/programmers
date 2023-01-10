c67c6049d663a6048174c119d2fccb46dd05db9a
wndudrla1011 <wndudrla1011@naver.com>
2023-01-10 16:33:27 +0900

    소수 찾기 완료

---
+++ src/main/java/level1/소수찾기/Solution2.java
@@ -0,0 +1,24 @@
+package level1.소수찾기;
+
+public class Solution2 {
+
+    public int solution(int n) {
+        int answer = 0;
+
+        for (int i = 1; i <= n; i++) {
+            int count = 0;
+            for (int j = 1; j <= i; j++) {
+                if (i % j == 0) count++;
+            }
+            if (count == 2) answer++;
+        }
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Solution2 s = new Solution2();
+        System.out.println(s.solution(10));
+    }
+
+}
