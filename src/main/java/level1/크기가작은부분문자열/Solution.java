6ede198cadb03641ebdda10f8d77d8dbd0d3b0e2
wndudrla1011 <wndudrla1011@naver.com>
2023-01-11 17:11:28 +0900

    크기가 작은 부분 문자열 완료

---
+++ src/main/java/level1/크기가작은부분문자열/Solution.java
@@ -0,0 +1,23 @@
+package level1.크기가작은부분문자열;
+
+public class Solution {
+
+    public int solution(String t, String p) {
+        int answer = 0;
+        long num = Long.parseLong(p);
+        int len = p.length();
+
+        for (int i = 0; i <= t.length() - len; i++) {
+            long diff = Long.parseLong(t.substring(i, i + len));
+            if (diff <= num) answer ++;
+        }
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        System.out.println(s.solution("10203", "15"));
+    }
+
+}
