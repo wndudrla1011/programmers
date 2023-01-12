48a4d2ae07f03432f74adb3401604cfa5f76ce98
wndudrla1011 <wndudrla1011@naver.com>
2023-01-12 16:20:11 +0900

    완주하지 못한 선수 완료

---
+++ src/main/java/level1/완주하지못한선수/Solution.java
@@ -0,0 +1,24 @@
+package level1.완주하지못한선수;
+
+import java.util.*;
+
+public class Solution {
+
+    public String solution(String[] participant, String[] completion) {
+        Arrays.sort(participant);
+        Arrays.sort(completion);
+
+        int i = 0;
+        for (i = 0; i < completion.length; i++) {
+            if (!participant[i].equals(completion[i])) break;
+        }
+
+        return participant[i];
+    }
+
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        System.out.println(s.solution(new String[]{"leo", "kiki", "eden"}, new String[]{"eden", "kiki"}));
+    }
+
+}
