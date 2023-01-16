a697e4e66a80b1db85ffb5180fc5330142457ea3
wndudrla1011 <wndudrla1011@naver.com>
2023-01-16 13:03:33 +0900

    신고 결과 받기 완료

---
+++ src/main/java/level1/신고결과받기/Solution.java
@@ -0,0 +1,51 @@
+package level1.신고결과받기;
+
+import java.util.*;
+
+public class Solution {
+
+    public int[] solution(String[] id_list, String[] report, int k) {
+        int[] answer = new int[id_list.length];
+
+        //중복 내역 제거
+        Set<String> newReport = new LinkedHashSet<>(Arrays.asList(report));
+
+        //신고 내역 정리
+        Map<String, Integer> map = new HashMap<>();
+        for (String s : newReport) {
+            String[] r = s.split(" ");
+            for (int i = 0; i < id_list.length; i++) {
+                if (r[0].equals(id_list[i])) {
+                    answer[i]++;
+                    break;
+                }
+            }
+            map.put(r[1], map.getOrDefault(r[1], 0) + 1);
+        }
+
+        //처리 결과
+        List<String> stopped_id = new ArrayList<>();
+        for (Map.Entry<String, Integer> entry : map.entrySet()) {
+            if (entry.getValue() >= k)
+                stopped_id.add(entry.getKey());
+        }
+
+        //알림 메일 횟수 계산
+        for (String s : newReport) {
+            String[] r = s.split(" ");
+            for (int i = 0; i < id_list.length; i++) {
+                if (r[0].equals(id_list[i]) && !stopped_id.contains(r[1]))
+                    answer[i]--;
+            }
+        }
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        System.out.println(Arrays.toString(s.solution(new String[]{"muzi", "frodo", "apeach", "neo"},
+                new String[]{"muzi frodo","apeach frodo","frodo neo","muzi neo","apeach muzi"}, 2)));
+    }
+
+}
