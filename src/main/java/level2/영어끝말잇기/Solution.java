e87ea83c5549c52c4d5999a29bc72cb4a4d37717
wndudrla1011 <wndudrla1011@naver.com>
2023-12-02 10:22:46 +0900

    영어 끝말잇기 수정

--- src/main/java/level2/영어끝말잇기/Solution.java
+++ src/main/java/level2/영어끝말잇기/Solution.java
@@ -13,8 +13,7 @@ public class Solution {
             char prev = words[i - 1].charAt(words[i - 1].length() - 1);
             char cur = words[i].charAt(0);
             if (prev != cur || dict.contains(words[i])) {
-                if ((i + 1) % n == 0) answer[0] = n;
-                else answer[0] = i % n + 1;
+                answer[0] = i % n + 1;
                 answer[1] = (int) Math.ceil((double) (i + 1) / n);
                 break;
             }
