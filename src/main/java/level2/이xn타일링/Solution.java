1ce02e1beaa9f91cb270932d6d3f9b1de62fda9f
wndudrla1011 <wndudrla1011@naver.com>
2024-01-08 13:07:57 +0900

    2xn 타일링 완료

---
+++ src/main/java/level2/이xn타일링/Solution.java
@@ -0,0 +1,21 @@
+package level2.이xn타일링;
+
+public class Solution {
+
+    public int solution(int n) {
+        int[] dp = new int[n + 1];
+
+        dp[1] = 1; dp[2] = 2;
+        for (int i = 3; i <= n; i++) {
+            dp[i] = (dp[i - 1] + dp[i - 2]) % 1000000007;
+        }
+
+        return dp[n];
+    }
+
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        System.out.println(s.solution(4));
+    }
+
+}
